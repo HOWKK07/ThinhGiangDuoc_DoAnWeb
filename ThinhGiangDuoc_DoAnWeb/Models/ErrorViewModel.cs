09198e403d2111e1e2d9511c1cbb11773eb86f78
NguyenThinh <thinhnguyenkk07@gmail.com>
2025-06-24 14:22:01 +0700

    them1

---
+++ ThinhGiangDuoc_DoAnWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThinhGiangDuoc_DoAnWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
